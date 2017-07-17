19e4ed86079a76578dd16cf1da2b675a24e06eab
Webs <weberswords@gmail.com>
2017-07-16 21:05:23 -0700

    Updated completed FizzBuzz

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
@@ -32,6 +32,19 @@ public class FizzBuzz {
     }
 
     private static void fizzBuzz() {
-
+        for(int i=1; i<101; i++) {
+            String output = "";
+            if(i%3 == 0) {
+                output += "Fizz";
+            }
+            if (i%5 == 0) {
+                output += "Buzz";
+            }
+            if (output == "") {
+                System.out.println(i);
+            } else {
+                System.out.println(output);
+            }
+        }
     }
 }
