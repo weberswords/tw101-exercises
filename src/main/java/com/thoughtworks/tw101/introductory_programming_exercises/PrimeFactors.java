ba903e8995ce56fe987709494e43ec583325292e
Webs <weberswords@gmail.com>
2017-07-23 17:44:42 -0700

    Completed PrimeFactors

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,16 +6,28 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
     public static void main(String[] args) {
         List<Integer> primeFactors = generate(30);
+        for (Integer x : primeFactors) {
+            System.out.print(x + " ");
+        }
     }
 
     private static List<Integer> generate(int n) {
-
-
-        return null;
+        List<Integer> primes = new ArrayList();
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                primes.add(i);
+                n /= i;
+                if (i != n) {
+                    generate(n);
+                }
+            }
+        }
+        return primes;
     }
 }
