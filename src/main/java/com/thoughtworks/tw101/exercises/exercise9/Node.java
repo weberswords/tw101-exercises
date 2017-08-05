2afa5a88b5c27d8429e9547971c9d3e2cb5f973b
StephWeb <weberswords@gmail.com>
2017-08-05 16:58:57 -0500

    Finished exercise 9!!!

--- src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise9/Node.java
@@ -1,18 +1,47 @@
 package com.thoughtworks.tw101.exercises.exercise9;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Node {
     private String name;
+    private Node leftChildNode;
+    private Node rightChildNode;
 
     public Node(String name) {
+
         this.name = name;
+        leftChildNode = null;
+        rightChildNode = null;
     }
+
 
     public void add(String nameOfNewNode) {
-    }
+        if (this.name.compareTo(nameOfNewNode) <= 0) {
+            if (rightChildNode == null) {
+                rightChildNode = new Node(nameOfNewNode);
+            } else {
+                rightChildNode.add(nameOfNewNode);
+            }
+        }
+        else {
+                if (leftChildNode == null) {
+                    leftChildNode = new Node(nameOfNewNode);
+                } else {
+                    leftChildNode.add(nameOfNewNode);
+                }
+            }
+        }
 
     public List<String> names() {
-        return null;
+        List<String> names = new ArrayList<>();
+        if (leftChildNode != null) {
+            names = leftChildNode.names();
+        }
+        names.add(name);
+        if (rightChildNode != null) {
+            names.addAll(rightChildNode.names());
+        }
+        return names;
     }
 }
